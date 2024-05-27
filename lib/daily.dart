066caa50a2af18198d0ba4fae7dfd662bd217c56
beyzayurdakan <beyzayurdakan@gmail.com>
2024-05-27 22:58:53 +0300

    edited daily page

--- lib/daily.dart
+++ lib/daily.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart'; // Ensure you have the intl package installed
 
 class ArtDetailsPage extends StatefulWidget {
   const ArtDetailsPage({super.key});
@@ -44,6 +45,10 @@ class _ArtDetailsPageState extends State<ArtDetailsPage> {
     DocumentSnapshot movementSnapshot = await movementRef.get();
     DocumentSnapshot museumSnapshot = await museumRef.get();
 
+    // Format the date
+    DateTime publishDate = artworkInfo['publishdate'].toDate();
+    String formattedDate = DateFormat('dd.MM.yyyy').format(publishDate);
+
     return {
       'image': artworkInfo['image'],
       'name': artworkInfo['name'],
@@ -52,6 +57,7 @@ class _ArtDetailsPageState extends State<ArtDetailsPage> {
       'artist': (artistSnapshot.data() as Map<String, dynamic>)['name'],
       'movement': (movementSnapshot.data() as Map<String, dynamic>)['name'],
       'museum': (museumSnapshot.data() as Map<String, dynamic>)['name'],
+      'formattedDate': formattedDate // Include formatted date in the return data
     };
   }
 
@@ -62,6 +68,7 @@ class _ArtDetailsPageState extends State<ArtDetailsPage> {
         title: Text('Daily Artwork'),
         backgroundColor: Colors.grey[300],
         elevation: 0,
+        automaticallyImplyLeading: false,
       ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: artworkData,
@@ -78,66 +85,96 @@ class _ArtDetailsPageState extends State<ArtDetailsPage> {
 
           Map<String, dynamic> data = snapshot.data!;
           return Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(15),
             child: ListView(
               children: [
-                Image.asset(data['image']),
-                SizedBox(height: 10),
-                Text(
-                  data['name'],
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
                 SizedBox(height: 10),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    GestureDetector(
-                      onTap: () => Navigator.pushNamed(context, '/artist', arguments: data['artist']),
-                      child: Chip(
-                        label: Text(data['artist']),
-                        backgroundColor: Colors.green[100],
-                      ),
-                    ),
-                    SizedBox(width: 10),
                     Text(
-                      data['year'],
+                      data['formattedDate'],
                       style: TextStyle(
+                        color: Colors.black,
                         fontSize: 16,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Text(
-                  data['description'],
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    GestureDetector(
-                    onTap: () => Navigator.pushNamed(context, '/museum', arguments: data['museum']),
-                    child: Chip(
-                      label: Text(data['museum']),
-                      backgroundColor: Colors.green[100],
                       ),
                     ),
                   ],
                 ),
                 SizedBox(height: 10),
-                Row(
-                  children: [
-                    GestureDetector(
-                  onTap: () => Navigator.pushNamed(context, '/movement', arguments: data['movement']),
-                  child: Chip(
-                    label: Text(data['movement']),
-                    backgroundColor: Colors.green[100],
+                Image.asset(data['image']),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200], // Light grey background
+                    borderRadius: BorderRadius.circular(10), // Rounded corners
                   ),
-                ),
-                  ],
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            data['name'],
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () => Navigator.pushNamed(context, '/artist', arguments: data['artist']),
+                            child: Chip(
+                              label: Text(data['artist']),
+                              backgroundColor: Colors.green[100],
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            data['year'],
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        data['description'],
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () => Navigator.pushNamed(context, '/museum', arguments: data['museum']),
+                            child: Chip(
+                              label: Text(data['museum']),
+                              backgroundColor: Colors.green[100],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () => Navigator.pushNamed(context, '/movement', arguments: data['movement']),
+                            child: Chip(
+                              label: Text(data['movement']),
+                              backgroundColor: Colors.green[100],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
