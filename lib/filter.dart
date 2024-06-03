f7b1c9592ada6b3ed15b3b6683c4764305cf402f
beyzayurdakan <beyzayurdakan@gmail.com>
2024-06-03 06:56:20 +0300

    filtre son

--- lib/filter.dart
+++ lib/filter.dart
@@ -156,66 +156,66 @@ class _FilterPageState extends State<FilterPage> {
     );
   }
 
-Widget buildFilterTile(String title, List<String> options) {
-  bool showMore = showMoreMap[title] ?? false;
-  int itemsPerRow = (MediaQuery.of(context).size.width / 100).floor(); // Ekran genişliğine göre her satırda kaç öğe gösterileceğini belirle
-  int initialDisplayCount = itemsPerRow * 2; // İlk gösterilecek öğe sayısı, iki satır
+  Widget buildFilterTile(String title, List<String> options) {
+    bool showMore = showMoreMap[title] ?? false;
+    int itemsPerRow = (MediaQuery.of(context).size.width / 100).floor(); // Ekran genişliğine göre her satırda kaç öğe gösterileceğini belirle
+    int initialDisplayCount = itemsPerRow * 2; // İlk gösterilecek öğe sayısı, iki satır
 
-  if (widget.category == 'movements') {
-    initialDisplayCount = 8; 
-  }
-  List<String> displayedOptions = showMore ? options : options.take(initialDisplayCount).toList();
+    if (widget.category == 'movements') {
+      initialDisplayCount = 4; 
+    }
+    List<String> displayedOptions = showMore ? options : options.take(initialDisplayCount).toList();
 
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(title,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-        Wrap(
-          spacing: 8.0,
-          runSpacing: 4.0,
-          children: displayedOptions
-              .map((option) => FilterChip(
-                    label: Text(option),
-                    selected: (title == 'Country' && option == selectedCountry) ||
-                              (title != 'Country' && selectedFilters.contains(option)),
-                    onSelected: (bool selected) {
-                      setState(() {
-                        if (selected) {
-                          if (title == 'Country') {
-                            selectedCountry = option;
-                            selectedFilters.clear(); // Clear city selection when a new country is selected
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: displayedOptions
+                .map((option) => FilterChip(
+                      label: Text(option),
+                      selected: (title == 'Country' && option == selectedCountry) ||
+                                (title != 'Country' && selectedFilters.contains(option)),
+                      onSelected: (bool selected) {
+                        setState(() {
+                          if (selected) {
+                            if (title == 'Country') {
+                              selectedCountry = option;
+                              selectedFilters.clear(); // Clear city selection when a new country is selected
+                            } else {
+                              selectedFilters.add(option);
+                            }
                           } else {
-                            selectedFilters.add(option);
+                            if (title == 'Country') {
+                              selectedCountry = null;
+                              selectedFilters.clear(); // Clear city selection when country selection is cleared
+                            } else {
+                              selectedFilters.remove(option);
+                            }
                           }
-                        } else {
-                          if (title == 'Country') {
-                            selectedCountry = null;
-                            selectedFilters.clear(); // Clear city selection when country selection is cleared
-                          } else {
-                            selectedFilters.remove(option);
-                          }
-                        }
-                      });
-                    },
-                  ))
-              .toList(),
-        ),
-        if (options.length > initialDisplayCount)
-          TextButton(
-            onPressed: () {
-              setState(() {
-                showMoreMap[title] = !showMore;
-              });
-            },
-            child: Text(showMore ? 'Show Less' : 'Show More'),
+                        });
+                      },
+                    ))
+                .toList(),
           ),
-      ],
-    ),
-  );
-}
+          if (options.length > initialDisplayCount)
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  showMoreMap[title] = !showMore;
+                });
+              },
+              child: Text(showMore ? 'Show Less' : 'Show More'),
+            ),
+        ],
+      ),
+    );
+  }
 
 
   Widget buildCityFilter(String country) {
@@ -236,15 +236,14 @@ Widget buildFilterTile(String title, List<String> options) {
     };
 
     Map<String, List<int>> movementPeriodYearRanges = {
-    '1300s': [1300, 1399],
-    '1400s': [1400, 1499],
-    '1500s': [1500, 1599],
-    '1600s': [1600, 1699],
-    '1700s': [1700, 1799],
-    '1800s': [1800, 1899],
-    '1900s': [1900, 1999]
-  };
-
+      '1300s': [1300, 1399],
+      '1400s': [1400, 1499],
+      '1500s': [1500, 1599],
+      '1600s': [1600, 1699],
+      '1700s': [1700, 1799],
+      '1800s': [1800, 1899],
+      '1900s': [1900, 1999]
+    };
 
     if (widget.category == 'artworks') {
       // Filter by periods as year ranges, if any
@@ -301,6 +300,14 @@ Widget buildFilterTile(String title, List<String> options) {
       // Remove duplicates, if any
       filteredArtworkNames = filteredArtworkNames.toSet().toList();
 
+      // Eğer filtreleme sonucunda hiçbir öğe bulunamadıysa, bir hata mesajı göster
+      if (filteredArtworkNames.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('There is no artwork for the selected filters.')),
+        );
+        return;
+      }
+
       // Navigate back and pass the filtered artwork names
       Navigator.pop(context, {
         'filteredNames': filteredArtworkNames,
@@ -332,6 +339,14 @@ Widget buildFilterTile(String title, List<String> options) {
 
       // Remove duplicates, if any
       filteredMuseumNames = filteredMuseumNames.toSet().toList();
+
+      // Eğer filtreleme sonucunda hiçbir öğe bulunamadıysa, bir hata mesajı göster
+      if (filteredMuseumNames.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('There is no museum for the selected filters.')),
+        );
+        return;
+      }
 
       // Navigate back and pass the filtered museum names
       Navigator.pop(context, {
@@ -382,13 +397,11 @@ Widget buildFilterTile(String title, List<String> options) {
       // Remove duplicates, if any
       filteredArtistNames = filteredArtistNames.toSet().toList();
 
-      // If no artists match the selected periods, return an empty list
-      if (filteredArtistNames.isEmpty && selectedPeriods.isNotEmpty) {
-        Navigator.pop(context, {
-          'filteredNames': [],
-          'selectedFilters': selectedFilters,
-          'selectedCountry': selectedCountry
-        });
+      // Eğer filtreleme sonucunda hiçbir öğe bulunamadıysa, bir hata mesajı göster
+      if (filteredArtistNames.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('There is no artists for the selected filters.')),
+        );
         return;
       }
 
@@ -399,42 +412,50 @@ Widget buildFilterTile(String title, List<String> options) {
         'selectedCountry': selectedCountry
       });
     }
-  if (widget.category == 'movements') {
-    // Filter by selected periods
-    List<String> selectedPeriods = selectedFilters.where((f) => periods.contains(f)).toList();
-    if (selectedPeriods.isNotEmpty) {
-      List<String> periodList = selectedPeriods.map((period) => period).toList();
-      queries.add(baseQuery.where('period', arrayContainsAny: periodList));
-    } else {
-      // If no period is selected, add the base query as a fallback
-      queries.add(baseQuery);
+
+    if (widget.category == 'movements') {
+      // Filter by selected periods
+      List<String> selectedPeriods = selectedFilters.where((f) => periods.contains(f)).toList();
+      if (selectedPeriods.isNotEmpty) {
+        List<String> periodList = selectedPeriods.map((period) => period).toList();
+        queries.add(baseQuery.where('period', arrayContainsAny: periodList));
+      } else {
+        // If no period is selected, add the base query as a fallback
+        queries.add(baseQuery);
+      }
+
+      // Execute the queries and combine the results
+      List<String> filteredMovementNames = [];
+      for (var query in queries) {
+        var snapshot = await query.get();
+        filteredMovementNames.addAll(snapshot.docs
+            .map((doc) {
+              var data = doc.data();
+              return data is Map<String, dynamic> ? data['name'] as String? : null;
+            })
+            .where((name) => name != null)
+            .cast<String>()
+            .toList());
+      }
+
+      // Remove duplicates, if any
+      filteredMovementNames = filteredMovementNames.toSet().toList();
+
+      // Eğer filtreleme sonucunda hiçbir öğe bulunamadıysa, bir hata mesajı göster
+      if (filteredMovementNames.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('There is no movements for the selected filters.')),
+        );
+        return;
+      }
+
+      // Navigate back and pass the filtered movement names
+      Navigator.pop(context, {
+        'filteredNames': filteredMovementNames,
+        'selectedFilters': selectedFilters,
+        'selectedCountry': selectedCountry
+      });
     }
-
-    // Execute the queries and combine the results
-    List<String> filteredMovementNames = [];
-    for (var query in queries) {
-      var snapshot = await query.get();
-      filteredMovementNames.addAll(snapshot.docs
-          .map((doc) {
-            var data = doc.data();
-            return data is Map<String, dynamic> ? data['name'] as String? : null;
-          })
-          .where((name) => name != null)
-          .cast<String>()
-          .toList());
-    }
-
-    // Remove duplicates, if any
-    filteredMovementNames = filteredMovementNames.toSet().toList();
-
-    // Navigate back and pass the filtered movement names
-    Navigator.pop(context, {
-      'filteredNames': filteredMovementNames,
-      'selectedFilters': selectedFilters,
-      'selectedCountry': selectedCountry
-    });
-  }
-
   }
 
   void resetFilters() {
